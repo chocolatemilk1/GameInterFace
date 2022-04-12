848d728c3a3782f48b1adaa0b2ebde3c65c0f145
404-29\kw <shl04289@naver.com>
2022-04-12 10:34:09 +0900

    Barrel 랜덤 텍스쳐 표현

---
+++ 2022_SpaceShooter/Assets/02.Scripts/BarrelCtrl.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelCtrl : MonoBehaviour
+{
+    public GameObject expEffect;
+
+    public Texture[] textures;
+    private new MeshRenderer renderer;
+
+    private Transform tr;
+    private Rigidbody rb;
+
+    private int hitCount = 0;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+
+        renderer = GetComponentInChildren<MeshRenderer>();
+
+        int idx = Random.Range(0, textures.Length);
+
+        renderer.material.mainTexture = textures[idx];
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("BULLET"))
+        {
+            if(++hitCount == 3)
+            {
+                ExpBarrel();
+            }
+        }
+    }
+
+    void ExpBarrel()
+    {
+        GameObject exp = Instantiate(expEffect, tr.position, Quaternion.identity);
+
+        Destroy(exp, 5.0f);
+
+        rb.mass = 1.0f;
+        rb.AddForce(Vector3.up * 1500.0f);
+
+        Destroy(gameObject, 3.0f);
+    }
+}
